b5361f0a9c2c135dccb46ab696ad754fe1316bf9
Kajetan Schweighofer <kai.schweighofer@gmx.at>
2020-11-23 17:18:12 +0100

    Setup of environment

---
+++ test_env.py
@@ -0,0 +1,16 @@
+import sys
+import torch
+import gym
+
+"""
+Just a view tests, to see if environment is usable and to check for some errors
+"""
+
+print (sys.version)
+print(torch.cuda.is_available())
+env = gym.make('Pong-v0')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) # take a random action
+env.close()
